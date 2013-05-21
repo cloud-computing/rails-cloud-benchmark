7d70125ebca620accbc1d2a6afc2917282aaf2d4
Shyam Habarakada <shyam.habarakada@gmail.com>
2013-05-21 09:52:38 -0700

    Initial commit of baseline rails 3.2 app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsCloudBenchmark::Application.config.secret_token = '74921897f87011b6bd4c51182b401632a357a3a868fe6de415f02f77ac966c347a5451e1e4809fccac1ec998dec00ab6a6d692a6cd4d2f24563866926ef2968e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RailsCloudBenchmark::Application.config.session_store :cookie_store, key: '_rails-cloud-benchmark_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RailsCloudBenchmark::Application.config.session_store :active_record_store
