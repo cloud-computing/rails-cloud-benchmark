c30a4b8a755da1570944bef65d114e6ec65398b5
Dan Allison <dan@danallison.org>
2013-05-22 17:10:50 -0700

    add seed data (same as rake db:populate)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+    new_users = FactoryGirl.create_list(:user, 1000)
+    
+    new_users.each do |user|
+      blog = FactoryGirl.create(:blog)
+      blog.users << user
+      posts = FactoryGirl.create_list(:post, 3, user: user, blog: blog)
+      posts.each do |post|
+        FactoryGirl.create_list(:comment, 3, user: user, post: post)
+      end
+    end
+    
