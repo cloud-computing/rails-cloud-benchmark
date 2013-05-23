8bab7e992487dfc5d72b7e959878c2dea77ee8ba
Dan Allison <dan@danallison.org>
2013-05-22 17:24:49 -0700

    add (commented out) attempts to get tests using seed data
    these attempts are not working. will continue to troubleshoot.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,4 @@
+#ENV["RAILS_ENV"] = "development"
 ENV["RAILS_ENV"] = "test"
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
@@ -10,4 +11,5 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  #load "#{Rails.root}/db/seeds.rb"
 end
